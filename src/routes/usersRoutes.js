89679526979ab4c2d52416b2311472f5054ea6c3
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-02-25 09:18:54 -0300

    refactor: adjust routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const usersControllers = require("../controllers/usersControllers");
 
-router.get("/", usersControllers.getAllUser);
-router.post("/", usersControllers.addUser);
-router.get("/:id", usersControllers.getUserById);
-router.put("/:id", usersControllers.updateUser);
-router.delete("/:id", usersControllers.deleteUser);
+router.get("/user", usersControllers.getAllUser);
+router.post("/user", usersControllers.addUser);
+router.get("/user/:id", usersControllers.getUserById);
+router.put("/user/:id", usersControllers.updateUser);
+router.delete("/user/:id", usersControllers.deleteUser);
 
 
 module.exports = router;
